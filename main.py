d19c4440b2230819376702807c24bbb514926748
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-10-10 21:04:16 +0000

    Atividade de banco de dados python concluida!

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class Aluno(Base):
     __tablename__ = "alunos"
 
     # Definindo atributos da tabela
-    ra = Column("R.A.", Integer, primary_key=True, autoincrement=True)
+    ra = Column("ra", Integer, primary_key=True, autoincrement=True)
     nome = Column("nome", String)
     idade = Column("idade", Integer)
     senha = Column("senha", String)
@@ -39,6 +39,8 @@ class Aluno(Base):
 # Criando tabela no banco de dados.
 Base.metadata.create_all(bind=db)
 
+os.system("cls || clear")
+
 # Salvar banco de dados
 for i in range(2):
      ra = input("Digite o seu ra: ")
@@ -48,7 +50,19 @@ for i in range(2):
 
      aluno = Aluno(ra=ra, nome=nome, idade=idade, senha=senha)
      session.add(aluno)
-     session.commit()    
+     session.commit()
+     print()
+
+print("\nListando alunos no banco de dados")
+lista_alunos = session.query(Aluno).all()
+
+for aluno in lista_alunos:
+    print(f"{aluno.ra} - {aluno.nome} - {aluno.idade} - {aluno.senha}")
+
+aluno = session.query(Aluno).filter_by(nome="João").first()
+session.delete(aluno)
+session.commit()
+print("\nUsuário deletado com sucesso!")
 
 # usuario = Usuario("Ruan", "Ruan@gmail.com", "123")
 # aluno = Aluno(ra=222, nome="Ruan", idade=22, senha="123")
@@ -57,7 +71,7 @@ for i in range(2):
 
 # Mostrando conteúdo do banco de dados.
 # SELECT * FROM Usuarios
-lista_usuarios = session.query(Aluno).all()
+lista_alunos = session.query(Aluno).all()
 
-for usuario in lista_usuarios:
+for aluno in lista_alunos:
     print(f"{aluno.ra} - {aluno.nome}  - {aluno.idade} - {aluno.senha}")
